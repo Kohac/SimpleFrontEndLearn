167c73b6a3ecb0163388d41da0ba446f2bb8b9e4
kohy44@seznam.cz <kohy44@seznam.cz>
2022-08-30 21:11:44 +0200

    Add project files.

---
+++ SimpleFrontEndLearn/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleFrontEndLearn.Context;
+using SimpleFrontEndLearn.Models;
+
+namespace SimpleFrontEndLearn.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> result = _context.Categories;
+            return View(result);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
